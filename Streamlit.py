979efa25c48ed41b6fd8c6e058232b1aa3419f58
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2024-01-22 15:28:58 +0100

    to_excell

--- Streamlit.py
+++ Streamlit.py
@@ -46,13 +46,13 @@ if check_password():
 
         def to_excell(df):
             output = BytesIO()
-            writer = pd.ExcelWriter(output, engine='xlsxwriter')
-            df.to_excel(writer, index=False, sheet_name='Sheet1')
-            workbook = writer.book
-            worksheet = writer.sheets['Sheet1']
-            format1 = workbook.add_format({'num_format': '0.00'}) 
-            worksheet.set_column('A:A', None, format1)  
-            writer.save()
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                df.to_excel(writer, index=False, sheet_name='Sheet1')
+                workbook = writer.book
+                worksheet = writer.sheets['Sheet1']
+                format1 = workbook.add_format({'num_format': '0.00'}) 
+                worksheet.set_column('A:A', None, format1)
+            
             processed_data = output.getvalue()
             return processed_data
         
