abf9b989de05995a41bf23c97f744585480c3559
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2024-01-22 14:00:29 +0100

    vds

--- Streamlit.py
+++ Streamlit.py
@@ -12,7 +12,7 @@ import locale
 #for lang in locale.windows_locale.values():
 #    st.write(lang)
 
-locale.setlocale(locale.LC_ALL, "da_DK") 
+#locale.setlocale(locale.LC_ALL, "da_DK") 
 
 st.set_page_config(layout="wide", page_title="Forside", page_icon="https://media.licdn.com/dms/image/C4E0BAQEwX9tzA6x8dw/company-logo_200_200/0/1642666749832?e=2147483647&v=beta&t=UiNzcE1RvJD3kHI218Al7omOzPLhHXXeE_svU4DIwEM")
 st.sidebar.image('https://via.ritzau.dk/data/images/00181/e7ddd001-aee3-4801-845f-38483b42ba8b.png')
@@ -21,4 +21,4 @@ st.write('Hello World')
 
 if check_password():
     st.success('Login success')
-    
+
