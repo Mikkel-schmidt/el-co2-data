2e4d99f064fddfdb4e1233ad023ef8d470d4b8b8
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2024-01-22 13:44:56 +0100

    Initial commit

---
+++ Streamlit.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from stqdm import stqdm
+from datetime import datetime
+
+from streamlit_folium import st_folium
+from streamlit_functions import select_tree, getMeterPoints, getMeterReadings, check_password
+from streamlit_tree_select import tree_select
+from streamlit_extras.app_logo import add_logo
+
+import locale
+#for lang in locale.windows_locale.values():
+#    st.write(lang)
+
+locale.setlocale(locale.LC_ALL, "da_DK") 
+
+st.set_page_config(layout="wide", page_title="Forside", page_icon="https://media.licdn.com/dms/image/C4E0BAQEwX9tzA6x8dw/company-logo_200_200/0/1642666749832?e=2147483647&v=beta&t=UiNzcE1RvJD3kHI218Al7omOzPLhHXXeE_svU4DIwEM")
+st.sidebar.image('https://via.ritzau.dk/data/images/00181/e7ddd001-aee3-4801-845f-38483b42ba8b.png')
+
+st.write('Hello World')
